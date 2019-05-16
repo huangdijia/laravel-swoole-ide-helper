64a44ece277c9d05c0910e2f1783ea68fa9a99a2
huangdijia <huangdijia@gmail.com>
2019-05-16 09:51:25 +0800

    first commit

---
+++ Commands/GenerateCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Huangdijia\SwooleIdeHelper\Commands;
+
+use Illuminate\Console\Command;
+use Swoole\IDEHelper\ExtensionDocument;
+
+class GenerateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ide-helper:swoole';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate autocompletion for swoole';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $doc = new ExtensionDocument('chinese', dirname(__DIR__) . '/output', dirname(__DIR__) . '/config');
+        $doc->export();
+        $this->info("IDE help files for Swoole {$doc->getVersion()} are generated successfully.");
+    }
+}
